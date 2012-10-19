aa1a18c538e1c4466a1328e61c6153f83231f858
Munaf Assaf <munaf.assaf@gmail.com>
2012-10-18 22:51:40 -0700

    Removed  option from content schema in user options

--- jquery.speedread.js
+++ jquery.speedread.js
@@ -15,8 +15,7 @@
             chunkTime: 1000,
             schema: {
                 headers: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ],
-                content: [ 'p', 'section' ],
-                ignore: [ 'table' ]
+                content: [ 'p', 'section' ]
             }
         };
 
